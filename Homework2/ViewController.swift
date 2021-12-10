3f704511e580107e75d97fc6a509a4abd8427e9c
DinaSultanova <95694312+DinaSultanova@users.noreply.github.com>
2021-12-10 14:21:56 +0300

    Initial Commit

---
+++ Homework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2
+//
+//  Created by Дина on 10.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
