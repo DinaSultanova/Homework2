17d185e0acd96905157e9f9441a4e0952afef91d
DinaSultanova <95694312+DinaSultanova@users.noreply.github.com>
2021-12-12 13:25:12 +0300

    view, 3 sliders and 3 labels

--- Homework2/ViewController.swift
+++ Homework2/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sliderBlue: UISlider!
+    @IBOutlet weak var sliderGreen: UISlider!
+    @IBOutlet weak var sliderRed: UISlider!
+    @IBOutlet weak var viewChanched: UIView!
+    
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        sliderRed.value = 0
+        sliderRed.minimumValue = 0
+        sliderRed.maximumValue = 1
+        sliderRed.minimumTrackTintColor = .red
+        
+        sliderGreen.value = 0
+        sliderGreen.minimumValue = 0
+        sliderGreen.maximumValue = 1
+        sliderGreen.minimumTrackTintColor = .green
+        
+        sliderBlue.value = 0
+        sliderBlue.minimumValue = 0
+        sliderBlue.maximumValue = 1
+        sliderBlue.minimumTrackTintColor = .blue
     }
 
-
+    @IBAction func sliderRedAction(_ sender: Any) {
+        redLabel.text = String(sliderRed.value)
+    }
+    @IBAction func sliderGreenAction(_ sender: Any) {
+        greenLabel.text = String(sliderGreen.value)
+    }
+    @IBAction func sliderBlueAction(_ sender: Any) {
+        blueLabel.text = String(sliderBlue.value)
+    }
 }
 
